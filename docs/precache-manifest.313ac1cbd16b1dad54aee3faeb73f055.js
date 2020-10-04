cbad895a7d65301a08e24f80ef498b1d09d6ca44
Dean <deaneryfb@gmail.com>
2020-10-04 00:07:07 -0600

    docs-added

---
+++ docs/precache-manifest.313ac1cbd16b1dad54aee3faeb73f055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f780583826debc259e9a20e7e2ef0a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf6ca05c6927656fcf16",
+    "url": "/static/css/main.c1896aef.chunk.css"
+  },
+  {
+    "revision": "e3e6ffffbea7ec6d5416",
+    "url": "/static/js/2.f9d13a11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9d13a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6ca05c6927656fcf16",
+    "url": "/static/js/main.a2cf77fa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
